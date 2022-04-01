f9bfd8b6b214d28e8a83100b1c31daa79757fa14
Andrew <andrew.dsgnr@gmail.com>
2022-04-01 17:53:22 +0300

    Initial Commit

---
+++ Shared/Power_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_ButtonApp.swift
+//  Shared
+//
+//  Created by Andrew Tanchuk on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Power_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
